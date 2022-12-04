c8fa83afd8bfee8511024060470c44cfb654cffb
minhtq13 <taminh596@gmail.com>
2022-12-04 16:49:22 +0700

    config variable scss

--- front-end/src/component/MenuAccount/MenuAccount.jsx
+++ front-end/src/component/MenuAccount/MenuAccount.jsx
@@ -6,49 +6,49 @@ import "./MenuAccount.scss";
 import { useNavigate } from "react-router-dom";
 
 const MenuAccount = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const items = [
-    {
-      key: "1",
-      label: <div className="drop-item">Thông tin tài khoản</div>,
-      onClick: () => {
-        navigate("/member");
-      },
-    },
-    {
-      key: "2",
-      label: <div className="drop-item">Thẻ thành viên</div>,
-    },
-    {
-      key: "3",
-      label: <div className="drop-item">Đăng xuất</div>,
-      onClick: () => {
-        console.log("123");
-      },
-    },
-  ];
+    const items = [
+        {
+            key: "1",
+            label: <div className="drop-item">Thông tin tài khoản</div>,
+            onClick: () => {
+                navigate("/member");
+            },
+        },
+        {
+            key: "2",
+            label: <div className="drop-item">Thẻ thành viên</div>,
+        },
+        {
+            key: "3",
+            label: <div className="drop-item">Đăng xuất</div>,
+            onClick: () => {
+                console.log("123");
+            },
+        },
+    ];
 
-  return (
-    <div>
-      <Dropdown
-        className="dropdown-menu-account"
-        placement="bottomRight"
-        menu={{
-          items,
-        }}
-        arrow={{
-          pointAtCenter: true,
-        }}
-      >
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            Xin chào : <span className="username">Username</span>
-            <DownOutlined />
-          </Space>
-        </a>
-      </Dropdown>
-    </div>
-  );
+    return (
+        <div>
+            <Dropdown
+                className="dropdown-menu-account"
+                placement="bottomRight"
+                menu={{
+                    items,
+                }}
+                arrow={{
+                    pointAtCenter: true,
+                }}
+            >
+                <a onClick={(e) => e.preventDefault()}>
+                    <Space>
+                        Xin chào : <span className="username">Username</span>
+                        <DownOutlined />
+                    </Space>
+                </a>
+            </Dropdown>
+        </div>
+    );
 };
 export default MenuAccount;
--- front-end/src/layout/DefaultLayout/Nav/index.jsx
+++ front-end/src/layout/DefaultLayout/Nav/index.jsx
@@ -1,93 +1,93 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { WrapperStyled } from "./styled";
 import logoBeta from "../../../asset/images/logoBeta.png";
 import { Menu } from "antd";
 import { AppstoreOutlined } from "@ant-design/icons";
 import MenuAccount from "../../../component/MenuAccount/MenuAccount";
+import "./index.scss";
 export default function Nav() {
-  const [cinemaName, setCinemaName] = useState("Beta Thanh Xuân");
-  function getItem(label, key, icon, children, type) {
-    return {
-      label,
-      key,
-      icon,
-      children,
-      type,
+    const [cinemaName, setCinemaName] = useState("Beta Thanh Xuân");
+    function getItem(label, key, icon, children, type) {
+        return {
+            label,
+            key,
+            icon,
+            children,
+            type,
+        };
+    }
+    const items = [
+        getItem(cinemaName, "sub1", <AppstoreOutlined />, [
+            getItem("Hà Nội", "sub2", null, [
+                getItem("Beta Thanh Xuân", "1"),
+                getItem("Beta Giải Phóng", "2"),
+            ]),
+            getItem("TPHCM", "sub3", null, [getItem("Beta Quang Trung", "3")]),
+            getItem("Bắc Giang", "sub4", null, [
+                getItem("Beta Bắc Giang", "4"),
+            ]),
+        ]),
+    ];
+    const handleClick = (e) => {
+        if (e.key === "1") return setCinemaName("Beta Thanh Xuân");
+        else if (e.key === "2") return setCinemaName("Beta Giải Phóng");
+        else if (e.key === "3") return setCinemaName("Beta Quang Trung");
+        else if (e.key === "4") return setCinemaName("Beta Bắc Giang");
     };
-  }
-  const items = [
-    getItem(cinemaName, "sub1", <AppstoreOutlined />, [
-      getItem("Hà Nội", "sub2", null, [
-        getItem("Beta Thanh Xuân", "1"),
-        getItem("Beta Giải Phóng", "2"),
-      ]),
-      getItem("TPHCM", "sub3", null, [getItem("Beta Quang Trung", "3")]),
-      getItem("Bắc Giang", "sub4", null, [getItem("Beta Bắc Giang", "4")]),
-    ]),
-  ];
-  const handleClick = (e) => {
-    if (e.key === "1") return setCinemaName("Beta Thanh Xuân");
-    else if (e.key === "2") return setCinemaName("Beta Giải Phóng");
-    else if (e.key === "3") return setCinemaName("Beta Quang Trung");
-    else if (e.key === "4") return setCinemaName("Beta Bắc Giang");
-  };
-  return (
-    <WrapperStyled>
-      <div className="wrapper-header">
-        <nav className="main-header">
-          <div className="nav-contain">
-            <Link to="/home">
-              <div className="logo-beta">
-                <img src={logoBeta} alt="" />
-              </div>
-            </Link>
+    return (
+        <div className="wrapper-header">
+            <nav className="main-header">
+                <div className="nav-contain">
+                    <Link to="/home">
+                        <div className="logo-beta">
+                            <img src={logoBeta} alt="" />
+                        </div>
+                    </Link>
 
-            <Menu
-              onClick={handleClick}
-              className="menu-cinemas"
-              style={{
-                width: 256,
-              }}
-              mode="vertical"
-              items={items}
-            />
-            <div className="nav-content">
-              <Link to="/home" className="nav-links">
-                Trang chủ
-              </Link>
+                    <Menu
+                        onClick={handleClick}
+                        className="menu-cinemas"
+                        style={{
+                            width: 256,
+                        }}
+                        mode="vertical"
+                        items={items}
+                    />
+                    <div className="nav-content">
+                        <Link to="/home" className="nav-links">
+                            Trang chủ
+                        </Link>
+                    </div>
+                    <div className="nav-content">
+                        <Link to="/schedule" className="nav-links">
+                            Lịch chiếu theo rạp
+                        </Link>
+                    </div>
+                    <div className="nav-content">
+                        <Link to="/movie" className="nav-links">
+                            Phim
+                        </Link>
+                    </div>
+                    <div className="nav-content">
+                        <Link to="/cinemas" className="nav-links">
+                            Rạp
+                        </Link>
+                    </div>
+                    <div className="nav-content">
+                        <Link to="/price" className="nav-links">
+                            Giá vé
+                        </Link>
+                    </div>
+                    <div className="nav-content">
+                        <Link to="/member" className="nav-links">
+                            Thành viên
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+            <div className="menu-account-component">
+                <MenuAccount />
             </div>
-            <div className="nav-content">
-              <Link to="/schedule" className="nav-links">
-                Lịch chiếu theo rạp
-              </Link>
-            </div>
-            <div className="nav-content">
-              <Link to="/movie" className="nav-links">
-                Phim
-              </Link>
-            </div>
-            <div className="nav-content">
-              <Link to="/cinemas" className="nav-links">
-                Rạp
-              </Link>
-            </div>
-            <div className="nav-content">
-              <Link to="/price" className="nav-links">
-                Giá vé
-              </Link>
-            </div>
-            <div className="nav-content">
-              <Link to="/member" className="nav-links">
-                Thành viên
-              </Link>
-            </div>
-          </div>
-        </nav>
-        <div className="nav-content">
-          <MenuAccount />
         </div>
-      </div>
-    </WrapperStyled>
-  );
+    );
 }

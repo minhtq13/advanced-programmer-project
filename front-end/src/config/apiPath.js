d6f4e7f4ea3d43d71b43228e263d2db4e031b4b7
minhtq13 <taminh596@gmail.com>
2022-12-18 14:56:57 +0700

    test api

--- front-end/src/config/apiPath.js
+++ front-end/src/config/apiPath.js
@@ -1,12 +1,13 @@
-export const BASE_URL = "https://61fe8c59a58a4e00173c98cc.mockapi.io";
+// export const BASE_URL = "https://61fe8c59a58a4e00173c98cc.mockapi.io";
+export const BASE_URL = "http://localhost:8080";
 
 export const apiPath = {
-  // Account
-  login: BASE_URL + "/api/user/login", // url api login
-  register: BASE_URL + "/api/user/signup", // url api register
-  //
-  getInfoCinema: BASE_URL + "/cinema",
-  getInfoTickets: BASE_URL + "/tickets",
-  getInfoUser: BASE_URL + "/users",
-  getInfoFilm: BASE_URL + "/film",
+    // Account
+    login: BASE_URL + "/api/user/login", // url api login
+    register: BASE_URL + "/api/user/signup", // url api register
+    //
+    getInfoCinema: BASE_URL + "/cinema",
+    getInfoTickets: BASE_URL + "/tickets",
+    getInfoUser: BASE_URL + "/users",
+    getInfoFilm: BASE_URL + "/film",
 };
--- front-end/src/pages/Movie/components/Menu.jsx
+++ front-end/src/pages/Movie/components/Menu.jsx
@@ -6,51 +6,54 @@ import ButtonBuyTicket from "../../../component/ButtonBuyTicket/ButtonBuyTicket"
 import AvatarFilm from "../../../component/AvatarFilm/AvatarFilm";
 
 const Menu = ({ items }) => {
-  const { infoFilm, getInfoFilmInFilmPage } = useFilm();
-  useEffect(() => {
-    if (infoFilm) {
-      getInfoFilmInFilmPage();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const { infoFilm, getInfoFilmInFilmPage } = useFilm();
+    useEffect(() => {
+        if (infoFilm) {
+            getInfoFilmInFilmPage();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    console.log(infoFilm);
 
-  return (
-    <div className="wrapper-movie-item">
-      {items.map((item) => {
-        const { id, title, img, vibe, time, date } = item;
-        return (
-          <div className="movie-item" key={id}>
-            <AvatarFilm
-              image={img}
-              width={227.5}
-              height={360}
-              borderRadius={12}
-            />
-            <div className="info-movie">
-              <div className="title-movie">{title}</div>
-              <div className="desc-movie">
-                <div className="vibe-movie detail">
-                  <div className="options">Thể loại : </div>
-                  <div className="info-vive">{vibe}</div>
-                </div>
-                <div className="time-movie detail">
-                  <div className="options">Thời lượng :</div>
-                  <div className="info-time">{time}</div>
-                </div>
-                <div className="date-movie detail">
-                  <div className="options">Ngày khởi chiếu :</div>
-                  <div className="info-date">{date}</div>
-                </div>
-              </div>
-            </div>
-            <div className="buy-ticket">
-              <ButtonBuyTicket />
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="wrapper-movie-item">
+            {items.map((item) => {
+                const { id, title, img, vibe, time, date } = item;
+                return (
+                    <div className="movie-item" key={id}>
+                        <AvatarFilm
+                            image={img}
+                            width={227.5}
+                            height={360}
+                            borderRadius={12}
+                        />
+                        <div className="info-movie">
+                            <div className="title-movie">{title}</div>
+                            <div className="desc-movie">
+                                <div className="vibe-movie detail">
+                                    <div className="options">Thể loại : </div>
+                                    <div className="info-vive">{vibe}</div>
+                                </div>
+                                <div className="time-movie detail">
+                                    <div className="options">Thời lượng :</div>
+                                    <div className="info-time">{time}</div>
+                                </div>
+                                <div className="date-movie detail">
+                                    <div className="options">
+                                        Ngày khởi chiếu :
+                                    </div>
+                                    <div className="info-date">{date}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="buy-ticket">
+                            <ButtonBuyTicket />
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Menu;

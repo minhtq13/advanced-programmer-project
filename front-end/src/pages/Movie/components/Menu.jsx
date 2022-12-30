9bfcc34043c54c050d037581907938db3d98fa81
MINHTQ3-VD\MinhTQ3 <minhtq3@rikkeisoft.com>
2022-12-30 13:56:02 +0700

    update 30/12

--- front-end/src/pages/Movie/components/Menu.jsx
+++ front-end/src/pages/Movie/components/Menu.jsx
@@ -7,65 +7,60 @@ import { useDispatch } from "react-redux";
 import { setDetailFilm } from "../../../redux/slices/appSlice";
 
 const Menu = ({ items }) => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    // const handleOnClick = () => {
-    //     navigate("/detail-film");
-    //     dispatch(setDetailFilm(item));
-    // };
-    return (
-        <div className="wrapper-movie-item">
-            {items.map((item) => {
-                const { id, name, img, category, duration, premiereDate } =
-                    item;
-                return (
-                    <div className="movie-item" key={id}>
-                        <AvatarFilm
-                            image={img}
-                            width={227.5}
-                            height={360}
-                            borderRadius={12}
-                        />
-                        <div className="info-movie">
-                            <div className="title-movie">
-                                <div
-                                    onClick={() => {
-                                        navigate("/detail-film");
-                                        dispatch(setDetailFilm(item));
-                                    }}
-                                >
-                                    {name}
-                                </div>
-                            </div>
-                            <div className="desc-movie">
-                                <div className="vibe-movie detail">
-                                    <div className="options">Thể loại : </div>
-                                    <div className="info-vive">{category}</div>
-                                </div>
-                                <div className="time-movie detail">
-                                    <div className="options">Thời lượng :</div>
-                                    <div className="info-time">
-                                        {duration} phút
-                                    </div>
-                                </div>
-                                <div className="date-movie detail">
-                                    <div className="options">
-                                        Ngày khởi chiếu :
-                                    </div>
-                                    <div className="info-date">
-                                        {premiereDate}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="buy-ticket">
-                            <ButtonBuyTicket />
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // const handleOnClick = () => {
+  //     navigate("/detail-film");
+  //     dispatch(setDetailFilm(item));
+  // };
+  return (
+    <div className="wrapper-movie-item">
+      {items.map((item) => {
+        const { id, name, img, category, duration, premiereDate } = item;
+        return (
+          <div className="movie-item" key={id}>
+            <div>
+              <AvatarFilm
+                image={img}
+                width={227.5}
+                height={360}
+                borderRadius={12}
+              />
+            </div>
+            <div className="info-movie">
+              <div className="title-movie">
+                <div
+                  onClick={() => {
+                    navigate("/detail-film");
+                    dispatch(setDetailFilm(item));
+                  }}
+                >
+                  {name}
+                </div>
+              </div>
+              <div className="desc-movie">
+                <div className="vibe-movie detail">
+                  <div className="options">Thể loại : </div>
+                  <div className="info-vive">{category}</div>
+                </div>
+                <div className="time-movie detail">
+                  <div className="options">Thời lượng :</div>
+                  <div className="info-time">{duration} phút</div>
+                </div>
+                <div className="date-movie detail">
+                  <div className="options">Ngày khởi chiếu :</div>
+                  <div className="info-date">{premiereDate}</div>
+                </div>
+              </div>
+            </div>
+            <div className="buy-ticket">
+              <ButtonBuyTicket />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
